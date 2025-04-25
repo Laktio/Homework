fd6d0fdd69f9c7b9c9cc9905d2d74f0d6288f696
Laktionov <ber_lak@mail.ru>
2025-04-25 11:36:04 +0500

    Created README.md

--- src/processing.py
+++ src/processing.py
@@ -1,11 +1,11 @@
 def filter_by_state(account_list: list, state: str ='EXECUTED') -> list:
     """функция принимает список словарей с данными по операциям по счетам и выводит
     список словарей только тех операций состояние ('state') которых вводному параметру"""
-    account_list_state = []
+    account_state = []
     for account in account_list:
         if account['state'] == state:
-            account_list_state.append(account)
-    return account_list_state
+            account_state.append(account)
+    return account_state
 
 
 # print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], 'CANCELED'))
