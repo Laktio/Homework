51d482484be14be2599fa3dbb3b53ae843224b99
Laktionov <ber_lak@mail.ru>
2025-05-04 13:16:08 +0500

    test_sort_by_date in test_processing created

--- src/processing.py
+++ src/processing.py
@@ -15,14 +15,24 @@ def filter_by_state(account_list: list, state: str ='EXECUTED') -> list:
 # print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
 #       {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
 #       {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-#       {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], 'CANCELED'))
-
+#       {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], 'invalid state'))
 
 def sort_by_date(account_list: list, sort_parametr=True) -> list:
     """функция принимает список словарей с данными по операциям по счетам и выводит
     список словарей отсортированный по дате"""
+    for account in account_list:
+        if 'date' not in account:
+            return "Данные не были введены!"
+        if len(account['date']) != 26:
+            return "Некорректный формат даты!"
     sorted_account_list = sorted(account_list, key=lambda account: account['date'], reverse=sort_parametr)
+    if sorted_account_list == []:
+        sorted_account_list = "Данные не были введены!"
+
     return sorted_account_list
 
 
-# print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], False))
+print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+      {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+      {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+      {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], False))
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,6 +1,7 @@
 import pytest
 
-from src.processing import filter_by_state
+from src.processing import filter_by_state, sort_by_date
+
 
 @pytest.mark.parametrize("account_list, state, exception", [
     ([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
@@ -35,5 +36,38 @@ def test_filter_by_state(account_list, state, exception):
     assert filter_by_state(account_list, state) == exception
 
 
-
+@pytest.mark.parametrize("account_list, sort_parametr, exception", [
+    ([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+      {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+      {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+      {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], False,
+     [{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+      {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+      {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+      {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}]),
+    ([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+      {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+      {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+      {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], True,
+     [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+      {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+      {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+      {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]),
+    ([],"","Данные не были введены!"),
+    ([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+      {'id': 939719570, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+      {'id': 594226727, 'state': 'CANCELED', 'date': '2019-07-03T18:35:29.512364'},
+      {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], True,
+     [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+      {'id': 939719570, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+      {'id': 594226727, 'state': 'CANCELED', 'date': '2019-07-03T18:35:29.512364'},
+      {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]),
+    ([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+      {'id': 939719570, 'state': 'EXECUTED', 'date': '19-07-03T18:35:29.512364'},
+      {'id': 594226727, 'state': 'CANCELED', 'date': '2019-07-03T18:35:29.512364'},
+      {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], True,
+     "Некорректный формат даты!")
+])
+def test_sort_by_date(account_list, sort_parametr, exception):
+    assert sort_by_date(account_list, sort_parametr) == exception
 
