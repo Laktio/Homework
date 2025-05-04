cc29f473270e565cd430ed3b8bb528ebe56969f4
Laktionov <ber_lak@mail.ru>
2025-05-04 19:15:20 +0500

    fixtures in conftest.py created

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -36,38 +36,32 @@ def test_filter_by_state(account_list, state, exception):
     assert filter_by_state(account_list, state) == exception
 
 
-@pytest.mark.parametrize("account_list, sort_parametr, exception", [
-    ([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-      {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-      {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-      {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], False,
-     [{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+def test_sort_by_date_fix(test_list_filter_state_1):
+    assert sort_by_date(test_list_filter_state_1, False) == [{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
       {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
       {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-      {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}]),
-    ([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-      {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-      {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-      {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], True,
-     [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+      {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}]
+
+
+def test_sort_by_date_fix(test_list_filter_state_1):
+     assert sort_by_date(test_list_filter_state_1, True) == [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
       {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
       {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-      {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]),
-    ([],"","Данные не были введены!"),
-    ([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+      {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
+
+
+def test_sort_by_date_fix(test_list_filter_state_2):
+    assert sort_by_date(test_list_filter_state_2, True) == "Данные не были введены!"
+
+
+def test_sort_by_date_fix(test_list_filter_state_3):
+    assert sort_by_date(test_list_filter_state_3, True) == [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
       {'id': 939719570, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
       {'id': 594226727, 'state': 'CANCELED', 'date': '2019-07-03T18:35:29.512364'},
-      {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], True,
-     [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-      {'id': 939719570, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-      {'id': 594226727, 'state': 'CANCELED', 'date': '2019-07-03T18:35:29.512364'},
-      {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]),
-    ([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-      {'id': 939719570, 'state': 'EXECUTED', 'date': '19-07-03T18:35:29.512364'},
-      {'id': 594226727, 'state': 'CANCELED', 'date': '2019-07-03T18:35:29.512364'},
-      {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], True,
-     "Некорректный формат даты!")
-])
-def test_sort_by_date(account_list, sort_parametr, exception):
-    assert sort_by_date(account_list, sort_parametr) == exception
+      {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+
+
+def test_sort_by_date_fix(test_list_filter_state_4):
+    assert sort_by_date(test_list_filter_state_4, True) == "Некорректный формат даты!"
+
 
