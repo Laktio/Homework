1d44b19d0b50eb8b16172e31192b5587c52a7be7
Laktionov <ber_lak@mail.ru>
2025-05-14 15:16:55 +0500

    decorators.py (log()), mylog.txt created

---
+++ src/decorators.py
@@ -0,0 +1,34 @@
+import logging
+
+
+def log(filename=None):
+    """Функция декоратор для логирования работы функций в файл (если в параметрах декоратора указан файл)
+    или в консоль"""
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            if filename != None:
+                logging.basicConfig(filename="mylog.txt", level=logging.DEBUG, filemode="a",
+                                format="%(asctime)s %(levelname)s %(message)s")
+                try:
+                    func(*args, **kwargs)
+                except TypeError:
+                    logging.debug(f"{func.__name__} error: TypeError. Inputs {args}, {kwargs}")
+                else:
+                    logging.debug(f"{func.__name__} is ok")
+            elif filename == None:
+                try:
+                    func(*args)
+                except TypeError:
+                    return(f"{func.__name__} error: TypeError. Inputs {args}, {kwargs}")
+                else:
+                    return(f"{func.__name__} is ok")
+        return wrapper
+    return decorator
+
+
+@log(filename="mylog.txt")
+def my_function(x, y):
+    """Проверочная функция работы декоратора"""
+    return x + y
+
+print(my_function(1, 2))
