1174c26aff7b6b847e68e1ad4cd1d21c7fc724a9
Laktionov <ber_lak@mail.ru>
2025-06-09 17:15:21 +0500

    category_filter in module filter created

--- src/filter.py
+++ src/filter.py
@@ -1,11 +1,10 @@
 import re
 from src.transactions_reader import csv_operations, excel_operation
 from src.utils import operations_transform
+from collections import Counter
 
-def search_filter(operations:str, search_string:str=str(input('''
-Введите статус, по которому необходимо выполнить фильтрацию. Доступные для фильтровки статусы: 
-EXECUTED, CANCELED, PENDING 
-'''))):
+
+def search_filter(operations:str, search_string:str):
     """Функция принимает список банковских операций (список словарей из функций operations_transform() из
      utils, csv_operations() из transactions_reader, excel_operation() из transactions_reader) и
      строку поиска и выводит список словарей содержащие данные из строки поиска"""
@@ -15,8 +14,6 @@ EXECUTED, CANCELED, PENDING
         operations_list = csv_operations(operations)
     elif 'xlsx' in operations:
         operations_list = excel_operation(operations)
-    else:
-        return "incorrect input data!"
     search_list = []
     for operation in operations_list:
         operation_state = str(operation.get('state'))
@@ -26,5 +23,35 @@ EXECUTED, CANCELED, PENDING
     return search_list
 
 
+# print(search_filter("../data/transactions_excel.xlsx", str(input('''
+#     Введите статус, по которому необходимо выполнить фильтрацию. Доступные для фильтровки статусы:
+#     EXECUTED, CANCELED, PENDING
+#     '''))))
 
-# print(search_filter("../data/transactions_excel.xlsx"))
+
+def category_filter(operations:str, categories:list):
+    """Функция принимает список банковских операций (список словарей из функций operations_transform() из
+     utils, csv_operations() из transactions_reader, excel_operation() из transactions_reader) и
+     список категорий операций и выводит словарь, в котором ключи — это названия категорий, а значения
+     — это количество операций в каждой категории"""
+    if 'json' in operations:
+        operations_list = operations_transform(operations)
+    elif 'csv' in operations:
+        operations_list = csv_operations(operations)
+    elif 'xlsx' in operations:
+        operations_list = excel_operation(operations)
+    else:
+        return "incorrect input data!"
+    categories_list = []
+    for operation in operations_list:
+        operation_state = str(operation.get('description'))
+        for category in categories:
+            search_result = re.search(category, operation_state, flags=re.IGNORECASE)
+            if search_result is not None:
+                categories_list.append(operation.get('description'))
+    counted_category_list = Counter(categories_list)
+    return counted_category_list
+
+
+# print(category_filter('../data/transactions.csv',
+#                       ['Перевод организации', 'Перевод с карты на карту']))
